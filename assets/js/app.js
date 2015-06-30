038eb016aa021aa3f20daa88f668f58d3968347c
Matt Arnold <matthew.arnold@m-w.co.uk>
2015-06-30 12:53:15 +0100

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,106 @@
+(function () {
+  var app = angular.module('AngularStart', ['ngRoute', 'LocalStorageModule', 'feeds', 'angular-loading-bar']);
+
+  app.config(function (localStorageServiceProvider) {
+    localStorageServiceProvider
+      .setPrefix('ngStartPage')
+      .setStorageCookie(0, '/')
+      .setNotify(true, true);
+  });
+
+  app.config(function($routeProvider) {
+    $routeProvider
+
+    // route for the home page
+    .when('/settings', {
+        templateUrl : '/assets/templates/settings.html',
+        controller  : 'settingsController'
+    });
+
+  });
+
+  app.controller("settingsController", function ($scope, localStorageService, $window) {
+    $scope.master = {};
+
+    $scope.updatePersonal = function(user) {
+      $scope.master = angular.copy(user);
+
+      var ob = {
+        Name: user.personalname,
+        URL: user.personalsite
+      };
+
+      submit('personalSite', ob);
+    };
+
+    $scope.updateWork = function(user) {
+      $scope.master = angular.copy(user);
+
+      var ob = {
+        Name: user.workname,
+        URL: user.worksite
+      };
+
+      submit('workSite', ob);
+    };
+
+    function submit(key, val) {
+      var existing = JSON.parse(localStorageService.get(key));
+
+      if (existing === null) {
+        existing = [];
+      }
+
+      existing.push(val);
+
+      $window.location.reload();
+
+      return localStorageService.set(key, JSON.stringify(existing));
+    }
+  });
+
+  app.controller("Utils", function ($scope) {})
+    .directive('currentTime', ['$interval', 'dateFilter',
+      function($interval, dateFilter) {
+        // return the directive link function. (compile function not needed)
+        return function(scope, element, attrs) {
+          var format = "h:mm a";  // date format
+          var stopTime; // so that we can cancel the time updates
+
+          // used to update the UI
+          function updateTime() {
+            element.text(dateFilter(new Date(), format));
+          }
+
+          // watch the expression, and update the UI on change.
+          scope.$watch(attrs.myCurrentTime, function(value) {
+            //format = value;
+            console.log(attrs);
+            updateTime();
+          });
+
+          stopTime = $interval(updateTime, 1000);
+
+          // listen on DOM destroy (removal) event, and cancel the next UI update
+          // to prevent updating time after the DOM element was removed.
+          element.on('$destroy', function () {
+            $interval.cancel(stopTime);
+          });
+        }
+      }]);
+
+  app.controller("PageListing", function ($scope, localStorageService) {
+
+    console.log(JSON.parse(localStorageService.get('personalSite')));
+
+    $scope.play_pages = (JSON.parse(localStorageService.get('personalSite')) === null) ? '' : JSON.parse(localStorageService.get('personalSite'));
+
+    console.log(JSON.parse(localStorageService.get('workSite')));
+
+    $scope.work_pages = (JSON.parse(localStorageService.get('workSite')) === null) ? '' : JSON.parse(localStorageService.get('workSite'));
+
+  });
+
+  
+
+}());
